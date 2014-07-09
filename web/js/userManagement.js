3b1806283a435d27451ca26c6b11b705d6e3694b
tools4origins <tools4origins@gmail.com>
2014-07-09 18:24:18 +0200

    Affichage liste utilisateur

--- web/js/userManagement.js
+++ web/js/userManagement.js
@@ -46,7 +46,7 @@ $(function(){
 			this.listenTo(this.model, 'change', this.render);
 	    },
 	    render: function () {
-	    
+	    	
 	        this.$el.html(this.template(this.model.toJSON()));
 	        this.input = this.$('.edit');
 	        return this;
