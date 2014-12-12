ef2163346010d234e69339155a30f54e56ab7f8b
Bruno Paccini <bruno@paccini.com>
2014-12-12 17:17:15 +0100

    adding main.js changes to the commit

--- web/js/backoffice/faq/main.js
+++ web/js/backoffice/faq/main.js
@@ -7,21 +7,14 @@ require.config({
     'backbone': 'bower_components/backbone/backbone',
     'bootstrap': 'bower_components/bootstrap/dist/js/bootstrap',
     'dateRangePicker': 'bower_components/bootstrap-daterangepicker/daterangepicker',
-<<<<<<< HEAD
-    'sortable': 'bower_components/jquery-sortable/source/js/jquery-sortable-min',
-    'wysihtml5': 'bower_components/wysihtml5/dist/wysihtml5-0.3.0.min',
-    'bootstrap-wysihtml5': 'bower_components/bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2',
-=======
     'sortable': 'bower_components/html5sortable/jquery.sortable',
     // 'wysihtml5': 'bower_components/wysihtml5/dist/wysihtml5-0.3.0.min',
     // 'bootstrap-wysihtml5': 'bower_components/bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2',
     'wysihtml5': 'wysiwyg/wysihtml5',
     'bootstrap-wysihtml5': 'wysiwyg/bootstrap-wysihtml5',
->>>>>>> faq refactoring api syncs
     'moment': 'bower_components/moment/moment',
     'todoTpl': 'todo',
     'flot': 'charts/flot/jquery.flot.min',
-    'validator': '../bundles/fpjsformvalidator/js/fp_js_validator',
     'app': 'backoffice/app'
   },
   shim: {
@@ -67,7 +60,6 @@ require([
   // Libraries required at bootstrap for the UI.
   "bootstrap",
   "todoTpl",
-  "validator",
   "sortable",
   "wysihtml5",
   "bootstrap-wysihtml5"
@@ -81,27 +73,6 @@ require([
 
   // save method to take collection url instead of the model root's Url.
   // Hacky but works.
-  Backbone.Model.prototype.saveFaq = function(key, value, options) {
-
-    options = options || {};
-    var attributes;
-
-    if (_.isObject(key) || key == null) {
-        attributes = key;
-    } else {
-        attributes = {};
-        attributes[key] = value;
-    }
-
-    var model = this;
-
-    if (model.collection && model.collection.url) {
-      options.url = model.collection.url;
-    }
-
-    return Backbone.Model.prototype.save.call(model, attributes, options);
-  };
-
   app.router = new Router();
   // Start app router
   Backbone.history.start();
