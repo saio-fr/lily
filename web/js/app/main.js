414eedd38b451cf28a8ee0d10108ae9d611b6350
Bruno Paccini <bruno@paccini.com>
2015-01-08 12:28:27 +0100

    first draft tracker refactoring

--- web/js/app/main.js
+++ web/js/app/main.js
@@ -55,6 +55,34 @@ require([
 
   'use strict';
 
+  /**
+   *
+   * Helpers and methods to communicate with the iframe;
+   *
+   */
+
+  function receiveMessage(event) {
+    // Do we trust the sender of this message?
+    if (event.origin !== "http://example.com:8080") {
+      return;
+    }
+
+    console.log(event.data);
+
+    // event.source is window.opener
+    // event.data is "hello there!"
+
+    // Assuming you've verified the origin of the received message (which
+    // you must do in any case), a convenient idiom for replying to a
+    // message is to call postMessage on event.source and provide
+    // event.origin as the targetOrigin.
+    event.source.postMessage("hi there yourself!  the secret response " +
+      "is: rheeeeet!",
+      event.origin);
+  }
+
+  window.addEventListener("message", receiveMessage, false);
+
   app.init = function() {
 
     $.ajaxPrefilter(function(options) {
