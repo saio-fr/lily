8069726a3ae9c38746e3441f28e5feda1ba8aa35
Bruno Paccini <bruno@paccini.com>
2015-01-15 11:37:39 +0100

    set Inactivity timeout back to 1200s

--- src/Lily/ChatBundle/Server/App/Connector.php
+++ src/Lily/ChatBundle/Server/App/Connector.php
@@ -24,24 +24,24 @@ class Connector implements WampServerInterface, MessageComponentInterface {
      * @var \Symfony\Component\DependencyInjection\ContainerInterface
      */
     protected $_container;
-    
+
     protected $config;
     protected $available;
 
-    public function __construct(WampServerInterface $app, $zmq, ContainerInterface $container) { 
-      
+    public function __construct(WampServerInterface $app, $zmq, ContainerInterface $container) {
+
     	  $context = new ZMQContext();
         $this->socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'pusher');
-        
+
         // Connect on the same server to save log
         $this->socket->connect("tcp://127.0.0.1:".$zmq);
-    
-        $this->container = $container;  
+
+        $this->container = $container;
         $this->cache = $this->container->get('aequasi_cache.instance.default');
-    
+
         $this->app = $app;
         $this->app->clients = new \SplObjectStorage();
-                  
+
     }
 
     public function onOpen(Conn $conn) {
@@ -49,7 +49,7 @@ class Connector implements WampServerInterface, MessageComponentInterface {
     }
 
     public function onSubscribe(Conn $conn, $topic) {
-	    
+
 	      $licence = $conn->WebSocket->request->getQuery()->get('licence');
 	      $newclient = true;
 
@@ -64,11 +64,11 @@ class Connector implements WampServerInterface, MessageComponentInterface {
   		          break;
             }
 	      }
-	      
+
 	      if ($newclient) {
-  	      
+
   	        echo 'new client';
-  	        
+
     		    $client = new \StdClass;
     		    $client->licence = $licence;
     		    $client->users = new \SplObjectStorage;
@@ -77,7 +77,7 @@ class Connector implements WampServerInterface, MessageComponentInterface {
     		    $client->operator = new Topic('operator/'.$licence);
     		    $this->app->clients->attach($client);
 	      }
-	      
+
         $this->app->onSubscribe($conn, $topic);
     }
 
@@ -89,13 +89,13 @@ class Connector implements WampServerInterface, MessageComponentInterface {
         $this->app->onPublish($conn, $topic, $event, $exclude, $eligible);
     }
 
-    public function onCall(Conn $conn, $id, $topic, array $params) {   
+    public function onCall(Conn $conn, $id, $topic, array $params) {
 	    // Get the conn licence
 	    $licence = $conn->WebSocket->request->getQuery()->get('licence');
       $this->app->onCall($conn, $id, $topic, $params);
-        
+
       // Check if there is operator available for the client
-      $this->isAvailable($licence);       
+      $this->isAvailable($licence);
     }
 
     public function onClose(Conn $conn) {
@@ -105,48 +105,48 @@ class Connector implements WampServerInterface, MessageComponentInterface {
     public function onError(Conn $conn, \Exception $e) {
         $this->app->onError($conn, $e);
     }
-    
+
     public function onMessage(Conn $from, $msg) {
     	$this->app->onMessage($from, $msg);
     }
-    
+
     public function config($licence) {
 
         $config = $this->cache->fetch($licence.'_config_app_chat');
 
-        if (!$config) {			
+        if (!$config) {
             // Get the client' entity manager
     	   		$connection = $this->container->get(sprintf('doctrine.dbal.%s_connection', 'client'));
-    		
+
     		    $refConn = new \ReflectionObject($connection);
     		    $refParams = $refConn->getProperty('_params');
     		    $refParams->setAccessible('public'); //we have to change it for a moment
-    		
+
     		    $params = $refParams->getValue($connection);
     		    $params['dbname'] = $licence;
-    		
+
     		    $refParams->setAccessible('private');
     		    $refParams->setValue($connection, $params);
-    				   	   		 	 	 
+
             $config = $this->container->get('doctrine')->getManager('client')
     				->getRepository('LilyBackOfficeBundle:ConfigChat')
     				->findOneById(1);
-    		   	  			  			
+
     		    $this->cache->save($licence.'_config_app_chat', $config, 0);
-		    }		
+		    }
         return $config;
     }
-    
+
     public function isAvailable($licence) {
-    	
+
         // For each clients
     		foreach ($this->app->clients as $client) {
       			if ($client->licence === $licence) {
-      			
+
       		    	$client->available = false;
-      		    	$operators = 0;	
+      		    	$operators = 0;
       		    	$queue = 0;
-      		
+
       		    	foreach($client->users as $user) {
       			    	  if ($user->type == 'operator' && $user->available) {
                           ++$operators;
@@ -155,22 +155,22 @@ class Connector implements WampServerInterface, MessageComponentInterface {
       			    		    }
       			    	  }
                     if ($user->type == 'visitor' && !$user->operator && !$user->closed) {
-      				          ++$queue;    	
+      				          ++$queue;
       				      }
       		    	}
-      		
+
       			    if (!$client->available && $operators > 0) {
         			      $condition1 = $client->config->getQueue();
         			      $condition2 = $queue < $client->config->getMaxQueue() * $operators;
       					    if ($condition1 && $condition2) $client->available = true;
                     else $client->available = false;
-      			    }			    
+      			    }
       			    // Set in the cache
-                $this->cache->save('chat_available_'.$licence, $client->available, 0); 
+                $this->cache->save('chat_available_'.$licence, $client->available, 0);
     		    }
     		}
     }
-   
+
    /**
     * Called every min
     */
@@ -178,51 +178,51 @@ class Connector implements WampServerInterface, MessageComponentInterface {
 
         // For each clients
         foreach ($this->app->clients as $client) {
-            
+
             // Test if visitor is still connected
             foreach ($client->users as $item) {
                 if ($item->type === 'visitor') {
-                      
+
                     // If the user is an visitor
-                    $condition1 = $item->lastConn < ( time() - 12 );
-                    $condition2 = $item->lastMsgTime < ( time() - 12 );
-                    
+                    $condition1 = $item->lastConn < ( time() - 1200 );
+                    $condition2 = $item->lastMsgTime < ( time() - 1200 );
+
                     if ($condition1 && $condition2) {
-                      
+
                         // If he is attached with an operator, decrease operator's chat counter
                         if ($item->operator) {
                             foreach ($client->users as $operator) {
-                                if ($operator->type === 'operator' && $operator->id == $item->operator) { 
-                                    // Decrease chat numbers	
+                                if ($operator->type === 'operator' && $operator->id == $item->operator) {
+                                    // Decrease chat numbers
                                     $operator->chats -= 1;
-            							      }			
-            						    }  	
+            							      }
+            						    }
     					          }
-      					          
+
           						  // Close the connection
                         $item->messages[] = array(
-                          'id' => uniqid(), 
-                          'from' => 'server', 
-                          'action' => 'inactivity', 
-                          'date' => time(), 
+                          'id' => uniqid(),
+                          'from' => 'server',
+                          'action' => 'inactivity',
+                          'date' => time(),
                           'msg' => "La connection a été terminé pour cause d'inactivité."
                         );
-                          
+
                         $item->topic->broadcast($item->messages);
-                          					          
+
                         $this->socket->send(json_encode(array(
-    				              'licence' => $client->licence, 
-                          'action' => 'log', 
+    				              'licence' => $client->licence,
+                          'action' => 'log',
                           'item' => $item)));
-              					
-              					// Detach the client	
+
+              					// Detach the client
               					$client->users->detach($item);
-          					}			
+          					}
 		            }
 			      }
 
             // Send users to client's operators
-            $client->operator->broadcast($this->app->toArray($client->users));		
-		    }   		     
+            $client->operator->broadcast($this->app->toArray($client->users));
+		    }
     }
 }
