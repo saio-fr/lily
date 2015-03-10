904df1eefa55fe94a10032183003a4f8d837864a
bruno <bruno@paccini.com>
2015-03-10 15:42:04 +0100

    added connectionLost modal in modal components

---
+++ web/js/components/modals/connectionLost.js
@@ -0,0 +1,42 @@
+/*======================================
+          MODAL CONNECTION LOST
+=======================================*/
+
+define(function(require) {
+
+  'use strict';
+
+  // Require CommonJS like includes
+  var Backbone = require('backbone'),
+    _ = require('underscore'),
+
+    // Object wrapper returned as a module
+    ConnectionLostModal;
+
+
+  ConnectionLostModal = Backbone.View.extend({
+
+    attributes: {
+      'tabindex': -1,
+      'role': 'dialog',
+      'aria-hidden': 'true',
+      'data-backdrop': 'true'
+    },
+    className: 'js-modal-connection-lost modal',
+    template: _.template($('#modalConnectionLostTpl').html()),
+
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      this.$el.appendTo('body');
+      return this;
+    }
+
+  });
+
+  return ConnectionLostModal;
+});
