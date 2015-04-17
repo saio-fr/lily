a678dec0ea191da5f329e889545483bf39c2bfad
neverlan <simonrapha6l@gmail.com>
2015-04-17 15:33:13 +0200

    Add App api + change in avi config

--- src/Lily/AppBundle/Entity/LogUnanswered.php
+++ src/Lily/AppBundle/Entity/LogUnanswered.php
@@ -5,12 +5,12 @@ namespace Lily\AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * LogRedirection
+ * Requete
  *
  * @ORM\Table()
- * @ORM\Entity(repositoryClass="Lily\AppBundle\Entity\LogRedirectionRepository")
+ * @ORM\Entity(repositoryClass="Lily\AppBundle\Entity\LogUnansweredRepository")
  */
-class LogRedirection
+class LogUnanswered
 {
     /**
      * @var integer
@@ -20,27 +20,42 @@ class LogRedirection
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
+
     /**
      * @var \DateTime
      *
      * @ORM\Column(name="date", type="datetime")
      */
     protected $date;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="media", type="string", length=7)
-     */
-    protected $media;
     
     /**
      * @var string
      *
-     * @ORM\Column(name="canal", type="string", length=7)
+     * @ORM\Column(name="session", type="string", length=255, nullable=true)
      */
-    protected $canal;
+    protected $session;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="query", type="string", length=255, nullable=true)
+     */
+    protected $query;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="media", type="string", length=10)
+     */
+    protected $media;
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->date = new \Datetime();
+    }
 
     /**
      * Get id
@@ -56,12 +71,12 @@ class LogRedirection
      * Set date
      *
      * @param \DateTime $date
-     * @return LogRedirection
+     * @return Requete
      */
     public function setDate($date)
     {
         $this->date = $date;
-
+    
         return $this;
     }
 
@@ -79,12 +94,12 @@ class LogRedirection
      * Set media
      *
      * @param string $media
-     * @return LogRedirection
+     * @return LogUnanswered
      */
     public function setMedia($media)
     {
         $this->media = $media;
-
+    
         return $this;
     }
 
@@ -99,25 +114,48 @@ class LogRedirection
     }
 
     /**
-     * Set canal
+     * Set session
      *
-     * @param string $canal
-     * @return LogRedirection
+     * @param string $session
+     * @return LogUnanswered
      */
-    public function setCanal($canal)
+    public function setSession($session)
     {
-        $this->canal = $canal;
-
+        $this->session = $session;
+    
         return $this;
     }
 
     /**
-     * Get canal
+     * Get session
      *
      * @return string 
      */
-    public function getCanal()
+    public function getSession()
     {
-        return $this->canal;
+        return $this->session;
+    }
+
+    /**
+     * Set query
+     *
+     * @param string $query
+     * @return LogUnanswered
+     */
+    public function setQuery($query)
+    {
+        $this->query = $query;
+    
+        return $this;
+    }
+
+    /**
+     * Get query
+     *
+     * @return string 
+     */
+    public function getQuery()
+    {
+        return $this->query;
     }
 }
